ea5301a7feb4c383c86b15b1e9e977c9eb588281
Anastasiya <anastasiya.axe@yandex.ru>
2023-07-22 10:38:00 +0300

    src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,8 @@
 import readlineSync from 'readline-sync';
 
 function askAboutName() {
-    const userName = readlineSync.question('May I have your name? ');
     console.log('Welcome to the Brain Games!');
+    const userName = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${userName}!`);
 }
 export default askAboutName;
